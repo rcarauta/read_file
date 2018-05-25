b0d83efedf4619a15206e1446c42ec7de4a3e3b7
Renato Carauta Ribeiro <rcarauta@unb.br>
2018-05-25 06:59:25 -0300

    Adição de criptografia

--- src/foo_sample_handler.erl
+++ src/foo_sample_handler.erl
@@ -3,9 +3,13 @@
 -export([init/2,terminate/3]).
 
 init(Req0, State) ->
+	CryptText = foo_start_handler:crypt_content("{\"other\": \"Web Service Sample!\"}"),
+
 	Req = cowboy_req:reply(200, #{
 		<<"content-type">> => <<"application/json">>
-	}, <<"{\"other\": \"Web Service Sample!\"}">>, Req0),
+	}, CryptText, Req0),
+	PlainText  = foo_start_handler:decripty_content(CryptText),
+	io:format("PlaintText  ~p~n~n",[PlainText]),
 	{ok, Req, State}.
 	
 terminate(_Reason, _Req, _State) ->
--- src/foo_start_handler.erl
+++ src/foo_start_handler.erl
@@ -1,6 +1,8 @@
 -module(foo_start_handler).
 
--export([init/2,terminate/3]).
+-export([init/2,terminate/3, decripty_content/1, crypt_content/1 ]).
+
+-define(JSON_LIB, jsx).
 
 init(Req0, State) ->
 	Req = cowboy_req:reply(200, #{
@@ -13,28 +15,45 @@ terminate(_Reason, _Req, _State) ->
 	
 	
 	
-%% decripty_content() ->
-%% 	CryptoText = ems_http_util:parse_barer_authorization_header(Authorization),
-%% 	PrivateKey = ems_util:open_file("/home/rcarauta/desenvolvimento/erlang/read_file/priv/catalog" ++  "/" ++ binary_to_list(<<"private_key.pem">>)),
-%% 	TextPlain = ems_util:decrypt_private_key(CryptoText,PrivateKey),
-%% 	?DEBUG("TextPlain ~p", [TextPlain]).
+decripty_content(CryptText) ->
+ 	CryptoText = parse_barer_authorization_header(CryptText),
+ 	PrivateKey = open_file("/home/rcarauta/desenvolvimento/erlang/read_file/priv/keys/private.pem"),
+ 	TextPlain = decrypt_private_key(CryptoText,PrivateKey),
+ 	TextPlain.
 	
 	
-%% crypt_content() ->
-%% 	ResponseData2 = ems_schema:prop_list_to_json(ResponseData),
-%% 	UserResponseData = lists:keyfind(<<"resource_owner">>, 1, ResponseData),
-%% 	PublicKey = ems_util:open_file("/home/rcarauta/desenvolvimento/erlang/read_file/priv/catalog" ++  "/" ++ binary_to_list(<<"public_key.pem">>)),
-%%  CryptoText = ems_util:encrypt_public_key(ResponseData2,PublicKey),
-%% 	CryptoBase64 = base64:encode(CryptoText).
+crypt_content(PlainText) ->
+ 	PublicKey = open_file("/home/rcarauta/desenvolvimento/erlang/read_file/priv/keys/public.pem"),
+    CryptoText = encrypt_public_key(PlainText,PublicKey),
+ 	CryptoBase64 = base64:encode(CryptoText),
+ 	CryptoBase64.
 	
 	
-%% encrypt_public_key(PlainText, PublicKey) ->
-%% 	[ RSAEntry2 ] = public_key:pem_decode(PublicKey),
-%% 	PubKey = public_key:pem_entry_decode( RSAEntry2 ),
-%% 	public_key:encrypt_public(PlainText, PubKey).
 	
-%% decrypt_private_key(CryptText,PrivateKey) ->
-%%     [ RSAEntry2 ] = public_key:pem_decode(PrivateKey),
-%% 	PrivKey = public_key:pem_entry_decode( RSAEntry2 ),
-%% 	Result =  public_key:decrypt_private(CryptText, PrivKey ),
-%% 	Result.
+encrypt_public_key(PlainText, PublicKey) ->
+ 	[ RSAEntry2 ] = public_key:pem_decode(PublicKey),
+ 	PubKey = public_key:pem_entry_decode( RSAEntry2 ),
+ 	public_key:encrypt_public(list_to_binary(PlainText), PubKey).
+	
+	
+decrypt_private_key(CryptText,PrivateKey) ->
+     [ RSAEntry2 ] = public_key:pem_decode(PrivateKey),
+ 	PrivKey = public_key:pem_entry_decode( RSAEntry2 ),
+ 	Result =  public_key:decrypt_private(CryptText, PrivKey, [{rsa_pad, rsa_pkcs1_padding}]),
+ 	Result.
+ 
+parse_barer_authorization_header(TextPlain) ->
+	base64:decode(TextPlain).
+	
+	
+open_file(FilePath) ->
+   {ok, PemBin2 } = file:read_file(FilePath),
+    PemBin2.
+	
+
+    
+
+    
+    
+
+
