976ca33758441dd16d82633e8f8c0f1c69f9bb6f
Renato Carauta Ribeiro <rcarauta@unb.br>
2018-11-05 09:03:59 -0200

    Adição do inset e select das tabelas do mnesia

--- src/database_connection.erl
+++ src/database_connection.erl
@@ -4,7 +4,7 @@
 
 -include_lib("stdlib/include/qlc.hrl").
 
--export([initiate_connection/1,start/0, start_mnesia/0, create_all_tables/0]).
+-export([initiate_connection/1,start/0, start_mnesia/0, create_all_tables/0, insert/1, find_all/1]).
 
 %% database_connection:initiate_connection("Driver={MySQL ODBC 5.1 Driver};Server=localhost;Database=algamoney;User=root;Password=travian10;").
 
@@ -21,3 +21,45 @@ start_mnesia() ->
 
 create_all_tables() ->
     mnesia:create_table(employee,[{attributes, record_info(fields, employee)}]).
+
+
+insert(Record) ->
+	RecordType = element(1, Record),
+	insert(RecordType, Record).
+
+
+insert(RecordType, Record) ->
+	F = fun() ->
+		case element(2, Record) of
+			undefined ->
+				Id = sequence(RecordType),
+				Record1 = setelement(2, Record, Id),
+				mnesia:write(Record1),
+				Record1;
+			Id -> 
+				case mnesia:read(RecordType, Id) of
+					[] -> mnesia:write(Record),
+						  Record;
+					_ -> {error, ealready_exist}
+				end
+		end
+	end,		
+	case mnesia:transaction(F) of
+		{atomic, Result} -> {ok, Result};
+		Error -> Error
+	end.
+
+
+
+find_all(Tab) ->
+	F = fun() ->
+		  qlc:e(
+			 qlc:q([X || X <- mnesia:table(Tab)])
+		  )
+	   end,
+	Records = mnesia:activity(async_dirty, F),
+	{ok, Records}.
+
+
+sequence(Name) ->  
+    mnesia:dirty_update_counter(sequence, Name, 1).
