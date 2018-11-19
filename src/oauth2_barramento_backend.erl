986295c7b210c7abe790f73224b75db56eff1cbe
Renato Carauta Ribeiro <rcarauta@unb.br>
2018-11-19 11:35:42 -0200

    Criação do backend do ouaht2

--- src/oauth2_barramento_backend.erl
+++ src/oauth2_barramento_backend.erl
@@ -2,7 +2,11 @@
 
 -export([start/0, stop/0, add_user/2, delete_user/1, add_client/2, add_client/3,
         delete_client/1, authenticate_username_password/3,  authenticate_client/3,
-        get_client_identity/2]).
+        get_client_identity/2, associate_access_code/3, associate_refresh_token/3, associate_access_token/3,
+        resolve_access_code/2, resolve_refresh_token/2, resolve_access_token/2, revoke_access_code/2,
+        revoke_access_token/2, revoke_refresh_token/2, get_redirection_uri/2, verify_redirection_uri/3,
+        verify_client_scope/3, verify_resowner_scope/3, verify_scope/3]).
+
 
 -record(client,{
     client_id :: binary(),
@@ -121,5 +125,61 @@ resolve_access_token(AccessToken, _) ->
         Error = {error, notfound} ->
             Error
     end.
+
+
+revoke_access_code(AccessCode, _AppContext) ->
+    revoke_access_token(AccessCode, _AppContext).
+
+
+revoke_access_token(AccessToken, _) ->
+    delete(?ACCESS_TOKEN_TABLE, AccessToken),
+    ok.
+
+
+revoke_refresh_token(_RefreshToken, _) ->
+    ok.
+
+
+get_redirection_uri(ClientId, _) ->
+    case get(?CLIENT_TABLE, ClientId) of 
+        {ok, #client{redirect_uri = RedirectUri}} ->
+            {ok, RedirectUri};
+        Error = {error, notfound} ->
+            Error
+    end.
+
+
+verify_client_scope(_ClientId, Scope, _) ->
+    {ok, Scope}.
+
+
+veriry_resowner_scope(_ResOwner, Scope, _) ->
+    {ok, Scope}.
+
+
+verify_scope(Scope, Scope, _) ->
+    {ok, Scope};
+verify_scope(_, _, _) ->
+    {error, invalid_scope}.
+
+
+%% Internal Functions
+
+get(Table, Key) ->
+    case ets:lookup(Table, Key) of 
+        [] ->
+            {error, notfound};
+        [{_Key, Value}] ->
+            {ok, Value}
+    end.
+
+
+put(Table, Key, Value) ->
+    ets:insert(Table, {Key, Value}),
+    ok.
+
+
+delete(Table, Key) ->
+    ets:delete(Table, Key).
 
 
